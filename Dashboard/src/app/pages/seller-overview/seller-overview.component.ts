cd8920c7e9b63ddb8c519b490a5dcf94478f07ce
fried.fritz@gmail.com <fried.fritz@gmail.com>
2017-12-20 17:39:42 +0100

    Händlerübersicht, Punktedarstellung

--- Dashboard/src/app/pages/seller-overview/seller-overview.component.ts
+++ Dashboard/src/app/pages/seller-overview/seller-overview.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-seller-overview',
   templateUrl: './seller-overview.component.html',
   styleUrls: ['./seller-overview.component.scss']
 })
-export class SellerOverviewComponent implements OnInit {
+export class SellerOverviewComponent {
+  activeValue: any;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor(private modalService: NgbModal) { }
+  
+
+    showLargeModal(sellerID : String) {
+      const activeModal = this.modalService.open(ModalComponent, { size: 'lg', container: 'nb-layout' });
+  
+      activeModal.componentInstance.modalSellername = sellerID; //Name mit SellerID aus Datenbank auslesen
+      activeModal.componentInstance.modalSellertext = sellerID; //Text mit SellerID aus Datenbank auslesen
+      activeModal.componentInstance.modalCollectedpoints = 5; //Anzahl gesammelter Items mit SellerID aus Datenbank auslesen (max. 10)
+
+    }
+
+    getModalValue(){
+      return this.activeValue;
+    }
+    showSmallModal() {
+      const activeModal = this.modalService.open(ModalComponent, { size: 'sm', container: 'nb-layout' });
+  
+      activeModal.componentInstance.modalHeader = 'Small Modal';
+    }
+  
+    showStaticModal() {
+      const activeModal = this.modalService.open(ModalComponent, {
+        size: 'sm',
+        backdrop: 'static',
+        container: 'nb-layout',
+      });
+  
+      activeModal.componentInstance.modalHeader = 'Static modal';
+      activeModal.componentInstance.modalContent = `This is static modal, backdrop click
+                                                      will not close it. Click × or confirmation button to close modal.`;
+    }
+  
 
 }
--- Dashboard/src/app/pages/seller-overview/seller-overview.module.ts
+++ Dashboard/src/app/pages/seller-overview/seller-overview.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { SellerOverviewComponent } from './seller-overview.component';
+import { ModalModule } from './modal/modal.module';
 
 @NgModule({
   imports: [
     CommonModule,
     ThemeModule,
+    ModalModule,
   ],
   declarations: [
     SellerOverviewComponent,
-  ]
+
+  ],  
 })
 export class SellerOverviewModule { }
