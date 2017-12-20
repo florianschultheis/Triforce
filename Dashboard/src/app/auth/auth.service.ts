94ebfba866ad8567de6a06c15dcbf80d5e899e3d
unknown <rasit.guven@live.de>
2017-12-20 17:30:57 +0100

    UserID und email übergeben

--- Dashboard/src/app/auth/auth.service.ts
+++ Dashboard/src/app/auth/auth.service.ts
@@ -2,9 +2,21 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import 'rxjs/add/operator/filter';
 import * as auth0 from 'auth0-js';
+import { JwtHelper } from 'angular2-jwt';
+
 
 @Injectable()
 export class AuthService {
+   
+jwtHelper: JwtHelper = new JwtHelper();
+
+  private decode(authResult: any) {
+    alert("in der methode: " + authResult.idToken);
+    var token = this.jwtHelper.decodeToken(authResult.idToken);
+    alert("unser dekodiertes token lautet: " + token.email);
+    //http post sende mail und UserID 
+    //http get ob datensatz vorhanden ist basierend auf e-mail
+  }
 
   auth0 = new auth0.WebAuth({
     clientID: 'u9ppezA7kI29KclGl7qlailQbwnwqu30',
@@ -12,7 +24,7 @@ export class AuthService {
     responseType: 'token id_token',
     audience: 'https://lolocode.eu.auth0.com/userinfo',
     redirectUri: 'http://localhost:8000/callback',     
-    scope: 'openid' 
+    scope: 'openid email' 
   });
 
   constructor(public router: Router) {}
@@ -24,9 +36,7 @@ export class AuthService {
   public handleAuthentication(): void {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
-        alert("hat geklappt");
         this.setSession(authResult);
-        alert("hat auch geklappt");
         this.router.navigate(['/']);
       } else if (err) {
         this.router.navigate(['/']);
@@ -41,6 +51,7 @@ export class AuthService {
     localStorage.setItem('access_token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
+    this.decode(authResult); 
   }
 
   public logout(): void {
